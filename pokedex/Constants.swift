8a3550c412eb9b94412edff7aa98cd23ff8c4f67
MPhill05 <Mark@192.168.0.80>
2016-05-12 02:38:03 -0500

    finish this pokedex app for now, going to change the tune and add more functionality later

---
+++ pokedex/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  pokedex
+//
+//  Created by Mark Phillips on 5/11/16.
+//  Copyright © 2016 Mark Phillips. All rights reserved.
+//
+
+import Foundation
+
+let URL_BASE = "http://pokeapi.co"
+let URL_POKEMON = "/api/v1/pokemon/"
+
+typealias DownloadComplete = () -> ()
